719ef07b3122c9d8280eef824e8475eebcb1d61f
Braun-Alex <72709229+Braun-Alex@users.noreply.github.com>
2022-10-01 03:13:09 +0000

    Update about.js

--- Shevaverse/Shevaplatform/pages/about.js
+++ Shevaverse/Shevaplatform/pages/about.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Header from "/components/Header/Header.js";
 import Footer from "/components/Footer/Footer.js";
